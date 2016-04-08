dc5a6615c04f80e90893b59a24e79fe9dba23cbe
Zhaomike <MikeZhao@zhaomikedeimac.local>
2016-04-08 09:23:53 +0800

    修复图片打印功能

--- MMPrinterDemo/ViewController.m
+++ MMPrinterDemo/ViewController.m
@@ -36,16 +36,16 @@
     NSTimeInterval timeout = 10;
     MMReceiptManager *manager = [[MMReceiptManager alloc] initWithHost:host port:port timeout:timeout];
     [manager basicSetting];
-//    [manager writeData_title:@"肯德基" Scale:scale_2 Type:MiddleAlignment];
-//    [manager writeData_items:@[@"收银员:001", @"交易时间:2016-03-17", @"交易号:201603170001"]];
-//    [manager writeData_line];
-//    [manager writeData_content:@[@{@"key01":@"名称", @"key02":@"单价", @"key03":@"数量", @"key04":@"总价"}]];
-//    [manager writeData_line];
-//    [manager writeData_content:@[@{@"key01":@"汉堡", @"key02":@"10.00", @"key03":@"2", @"key04":@"20.00"}, @{@"key01":@"炸鸡", @"key02":@"8.00", @"key03":@"1", @"key04":@"8.00"}]];
-//    [manager writeData_line];
-//    [manager writeData_items:@[@"支付方式:现金", @"应收:28.00", @"实际:30.00", @"找零:2.00"]];
-    UIImage *qrImage = [MMQRCode qrCodeWithString:@"hello" logoName:@"kfc.gif" size:200];
-    [manager writeData_image:qrImage alignment:MiddleAlignment maxWidth:200];
+    [manager writeData_title:@"肯德基" Scale:scale_2 Type:MiddleAlignment];
+    [manager writeData_items:@[@"收银员:001", @"交易时间:2016-03-17", @"交易号:201603170001"]];
+    [manager writeData_line];
+    [manager writeData_content:@[@{@"key01":@"名称", @"key02":@"单价", @"key03":@"数量", @"key04":@"总价"}]];
+    [manager writeData_line];
+    [manager writeData_content:@[@{@"key01":@"汉堡", @"key02":@"10.00", @"key03":@"2", @"key04":@"20.00"}, @{@"key01":@"炸鸡", @"key02":@"8.00", @"key03":@"1", @"key04":@"8.00"}]];
+    [manager writeData_line];
+    [manager writeData_items:@[@"支付方式:现金", @"应收:28.00", @"实际:30.00", @"找零:2.00"]];
+    UIImage *qrImage = [MMQRCode qrCodeWithString:@"www.google.cn" logoName:@"kfc.gif" size:400];
+    [manager writeData_image:qrImage alignment:MiddleAlignment maxWidth:400];
     [manager openCashDrawer];
     [manager printReceipt];
     
